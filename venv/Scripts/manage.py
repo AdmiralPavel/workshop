b8609ffa265696f3d3e54ab7e1772cb5b19e9668
AdmiralPavel <admiralpavel99@gmail.com>
2020-03-23 16:09:19 +0300

    Something working

---
+++ venv/Scripts/manage.py
@@ -0,0 +1,8 @@
+#!C:\Users\AdmiralPavel\PycharmProjects\barbershop_server\venv\Scripts\python.exe
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
