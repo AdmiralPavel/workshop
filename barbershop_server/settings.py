2bdad4d4814ae9e06a0a12c13c53203e7eef5389
AdmiralPavel <admiralpavel99@gmail.com>
2020-04-04 02:05:54 +0300

    Deploy on Heroku attempt #1

--- barbershop_server/settings.py
+++ barbershop_server/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
